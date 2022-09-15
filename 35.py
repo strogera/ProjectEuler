cb728ee92de4fec6732e4a42cb558cef7a50ca4b
strogera <26325577+strogera@users.noreply.github.com>
2022-09-15 19:19:31 +0300

    35

---
+++ 35.py
@@ -0,0 +1,32 @@
+from math import sqrt
+
+
+def isPrime(n):
+    if n == 1:
+        return True
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+seenPrime = set()
+seenNotPrime = set()
+res = []
+for x in range(2, 1000000):
+    allPrime = True
+    for r in range(len(str(x))):
+        rot = str(x)[r:] + str(x)[:r]
+        if rot in seenNotPrime:
+            allPrime = False
+            break
+        if rot not in seenPrime:
+            if isPrime(int(rot)):
+                seenPrime.add(int(rot))
+            else:
+                allPrime = False
+                seenNotPrime.add(rot)
+                break
+    if allPrime:
+        res.append(x)
+print(len(res))
