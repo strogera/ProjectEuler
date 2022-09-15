2f1a39139c6ed1d8a3e82fccc98509245288607d
strogera <26325577+strogera@users.noreply.github.com>
2022-09-15 19:59:18 +0300

    35 minor fix

--- 35.py
+++ 35.py
@@ -3,7 +3,7 @@ from math import sqrt
 
 def isPrime(n):
     if n == 1:
-        return True
+        return False
     for i in range(2, int(sqrt(n)) + 1):
         if n % i == 0:
             return False
